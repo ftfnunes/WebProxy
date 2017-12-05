ac13240f8fcb6f7330b19f07c7b37dc17bd162d1
Unknown <140039678@aluno.unb.br>
2017-12-05 04:21:18 -0200

    Arguments in main

--- include/WebCache.h
+++ include/WebCache.h
@@ -23,7 +23,7 @@
 #define CACHE_CONTROL_HEADER "Cache-Control"
 #define PRIVATE_DIRECTIVE "private"
 #define NO_CACHE_DIRECTIVE "no-cache"
-#define CACHED_RESPONSE_LIFETIME 5 //In minutes
+#define CACHED_RESPONSE_LIFETIME 2 //In minutes
 #define SIZE_OF_MESSAGE 500
 #define CACHE_PATH "./cache" //Nao deve terminar com /
 #define MAX_CACHE_SIZE 9000000
